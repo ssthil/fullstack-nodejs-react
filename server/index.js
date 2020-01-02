257ead89f1fb06f3657be705eb1fef30f661f898
Senthil Sivaramakrishnan <senthilsivaramakrishnan@Senthils-MacBook-Pro.local>
2020-01-02 21:28:12 +0100

    UPDATED appid and nowignore

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const accessKey = {
   },
 }; */
 
-const appid = 'ed973fdb30d0fffb8da125f32e7c1f25';
+const appid = config.open_weather_api.appid;
 
 // @ts-ignore
 // axios.defaults.headers.common['Authorization'] = `Bearer ${TOKEN}`;
